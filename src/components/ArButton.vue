20ecf721ffbac32f52d205dc6e81f2df884538ac
Ben <bbjork88@gmail.com>
2023-02-23 16:14:16 +0100

    Added AR button

---
+++ src/components/ArButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="ar-wrapper">
+        <button class="ar-button">Ta bild med AR</button>
+        <p>Genom att använda augmented reality (AR) kan man se en modell av en container i vekrligheten och även placera den där man önskar.</p>
+    </div>
+</template>
+
+<style>
+.ar-wrapper {
+    display: flex;
+    flex-flow: column wrap;
+}
+
+.ar-button {
+    box-shadow: #00000020 10px 10px 40px;
+    cursor: pointer;
+    font-size: 1.5em;
+    min-width: 250px;
+    padding: 15px;
+    margin-bottom: 5px;
+    margin-top: 50px;
+    border: none;
+    border-radius: 40px;
+    background-color: #2c84d0;
+    color: white;
+}
+
+.ar-wrapper p {
+    color: rgba(0,0,0,0.5);
+    font-size: 14px;
+
+}
+</style>
