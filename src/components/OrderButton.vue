9834d6b736b8fddbf6653b14730e4185ca07de58
GenieMini <zakaharif@hotmail.se>
2023-05-17 01:07:28 +0200

    email send img

--- src/components/OrderButton.vue
+++ src/components/OrderButton.vue
@@ -51,8 +51,7 @@ export default defineComponent({
             const markerLink = "https://www.google.com/maps/search/?api=1&query="+
             `${payloadStore.containerMarker.value.lat},`+
             `${payloadStore.containerMarker.value.lng}`;
-            const imageSrc = "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=5";
-
+            
             return {
                 to_email: adminEmail,
                 deliveryDate: payloadStore.deliveryDate.value,
@@ -65,7 +64,7 @@ export default defineComponent({
                 address: payloadStore.address.value,
                 zipcode: payloadStore.zipCode.value,
                 city: payloadStore.city.value,
-                imageSrc: imageSrc,
+                message: payloadStore.message.value,
                 title: payloadStore.selectedContainer.value?.title || "",
                 length: payloadStore.selectedContainer.value?.length || 0,
                 width: payloadStore.selectedContainer.value?.width || "",
